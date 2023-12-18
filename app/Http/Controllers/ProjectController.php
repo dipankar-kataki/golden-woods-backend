4b3b7a11fada2f0c6f658bb2c8f58de8270677e7
jay-ekodus <jsardar@ekodusinc.com>
2023-12-18 13:29:55 +0530

    project within reach changed to text-2

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -95,7 +95,7 @@ class ProjectController extends Controller
             if (!$project) {
                 return response()->json(['message' => 'Project not found'], 404);
             }
-            // $project->flatConfig = json_decode($project->flatConfig);
+            $project->flatConfig = json_decode($project->flatConfig);
             $project->withinReach = json_decode($project->withinReach);
 
             // Continue processing or return the response with project and gallery details        
