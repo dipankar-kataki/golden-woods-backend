087fcdb03e5730dc9836ec6bf28456d401ab9652
dipankar kataki <dipankar.kataki@ekodusinc.com>
2024-03-13 18:10:04 +0530

    ck editor added inside blog

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('admin.dashboard.dashboard');
+        $blogs_count = Blog::count();
+        return view('admin.dashboard.dashboard')->with(['blogs_count' => $blogs_count]);
     }
 }
