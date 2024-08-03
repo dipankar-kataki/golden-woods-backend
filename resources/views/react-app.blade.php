39f5b52d8ecc225ac7da8d57ad70faac9de25d96
jay-ekodus <jsardar@ekodusinc.com>
2024-08-03 14:18:45 +0530

    added new build-4

--- resources/views/react-app.blade.php
+++ resources/views/react-app.blade.php
@@ -12,7 +12,7 @@
 <body>
     <noscript>You need to enable JavaScript to run this app.</noscript>
     <div id="root"></div>
-    <script src="{{ asset('dist/assets/index-2e000b24.js') }}"></script>
+    <script src="{{ asset('dist/assets/index-42593166.js') }}"></script>
 </body>
 
 </html>
