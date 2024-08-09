bb75f8ae69ba78d091b351d74715ff6d43ca1ae2
jay-ekodus <jsardar@ekodusinc.com>
2024-08-09 12:11:32 +0530

    added regex-5

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
                     if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
                         return $this->error('Oops! Invalid credentials', null, 400);
                     } else {
-                        return $this->success('Great! Sign in successful', 'admin/dashboard', 200);
+                        return $this->success('Great! Sign in successful', '/admin/dashboard', 200);
                     }
                 }
 
