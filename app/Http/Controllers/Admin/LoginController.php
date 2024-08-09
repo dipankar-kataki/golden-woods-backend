0a7a5e8f12fc801f3dcdc68868fdb4cf9f664c0f
jay-ekodus <jsardar@ekodusinc.com>
2024-08-09 11:39:52 +0530

    added regex-2

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -11,27 +11,28 @@ use Illuminate\Support\Facades\Validator;
 class LoginController extends Controller
 {
     use AjaxResponser;
-    public function login(Request $request){
-        if($request->isMethod('get')){
+    public function login(Request $request)
+    {
+        if ($request->isMethod('get')) {
             return view('admin.auth.login');
-        }else{
-            try{
-                $validator = Validator::make($request->all(),[
+        } else {
+            try {
+                $validator = Validator::make($request->all(), [
                     'email' => 'required|email',
                     'password' => 'required'
                 ]);
 
-                if($validator->fails()){
-                    return $this->error('Oops! '.$validator->errors()->first(), null, 400);
-                }else{
-                    if(!Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+                if ($validator->fails()) {
+                    return $this->error('Oops! ' . $validator->errors()->first(), null, 400);
+                } else {
+                    if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
                         return $this->error('Oops! Invalid credentials', null, 400);
-                    }else{
-                        return $this->success('Great! Sign in successful', '/admin/dashboard', 200);
+                    } else {
+                        return $this->success('Great! Sign in successful', route('admin.dashboard'), 200);
                     }
                 }
 
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 return $this->error('Oops! Something went wrong.', null, 500);
             }
         }
