dd6bf2004874dbc561bd5a501cd4652ddacd74a3
jay-ekodus <jsardar@ekodusinc.com>
2024-08-17 11:00:29 +0530

    added -5

--- resources/views/react-app.blade.php
+++ resources/views/react-app.blade.php
@@ -12,7 +12,7 @@
 <body>
     <noscript>You need to enable JavaScript to run this app.</noscript>
     <div id="root"></div>
-    <script src="{{ asset('dist/assets/index-63055a65.js') }}"></script>
+    <script src="{{ asset('dist/assets/index-4604cc44.js') }}"></script>
 </body>
 
 </html>
