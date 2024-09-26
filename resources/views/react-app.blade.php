b7e60eef6510d33e4e56b300fb6886b6ca6aeed6
jay-ekodus <jay.sardar@zycelltech.com>
2024-09-26 13:07:32 +0530

    add

--- resources/views/react-app.blade.php
+++ resources/views/react-app.blade.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>React App</title>
-    <link rel="stylesheet" href="{{ asset('dist/assets/index-8d891609.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/assets/index-aa086846.css') }}">
 
 </head>
 
 <body>
     <noscript>You need to enable JavaScript to run this app.</noscript>
     <div id="root"></div>
-    <script src="{{ asset('dist/assets/index-a7adf304.js') }}"></script>
+    <script src="{{ asset('dist/assets/index-5927bc3b.js') }}"></script>
 </body>
 
 </html>
